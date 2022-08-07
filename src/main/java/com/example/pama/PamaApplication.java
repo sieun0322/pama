df5fc1cb52215b792aa8e84bef909ca6886bd4c1
zion <lce322@naver.com>
2022-08-07 23:31:48 +0900

    Test Spring Boot 내장서버

--- src/main/java/com/example/pama/PamaApplication.java
+++ src/main/java/com/example/pama/PamaApplication.java
@@ -8,10 +8,12 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class PamaApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(PamaApplication.class, args);
+		/*
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("local");
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
@@ -24,7 +26,7 @@ public class PamaApplication {
 		} finally {
 			em.close();
 		}
-		emf.close();
+		emf.close();*/
 
 	}
 	private static void logic(EntityManager em){
---
+++ src/main/java/com/example/pama/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.pama.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+}
