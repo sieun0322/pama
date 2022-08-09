f853a769510964c357d8c9a2a0366fe278a80f87
zion <lce322@naver.com>
2022-08-09 21:17:24 +0900

    security 설치

---
+++ src/main/java/com/example/pama/security/WebSecurity.java
@@ -0,0 +1,55 @@
+package com.example.pama.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.servlet.Filter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    //private UserService userService;
+    //private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    public WebSecurity(Environment env){
+        this.env = env;
+        //this.userService = userService;
+        //this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        //http.authorizeRequests().antMatchers("/**").permitAll()
+        http.authorizeRequests().antMatchers("/**")
+                //.hasIpAddress("127.0.0.1")
+                .permitAll()
+                .and()
+                //.addFilter(getAuthenticationFilter())
+        ;
+        http.headers().frameOptions().disable();//frame 으로 구별되어 있는 페이지 오류확인
+    }
+/*
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManagerBean(),userService,env);
+        //authenticationFilter.setAuthenticationManager(authenticationManager());
+        return authenticationFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }*/
+}
