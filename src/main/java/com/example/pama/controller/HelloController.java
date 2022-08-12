43331c79c138376d4d685189b0d60eb76bd91d53
zion <lce322@naver.com>
2022-08-12 23:54:41 +0900

    thymeleaf 템플릿 엔진 추가

--- src/main/java/com/example/pama/controller/HelloController.java
+++ src/main/java/com/example/pama/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.pama.controller;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,9 @@ public class HelloController {
     @RequestMapping("/")
     public String home(Model model) {
         return "home";
+    }
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "loginForm";
     }
 }
